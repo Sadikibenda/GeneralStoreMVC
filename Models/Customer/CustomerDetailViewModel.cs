47472cb700eb2d8d891ea79ef81c859f8713ebc4
Sadiki Benda <Sadiki.benda1@gmail.com>
2024-03-20 19:56:48 -0400

    First commit

---
+++ Models/Customer/CustomerDetailViewModel.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GeneralStoreMVC.Models.Transaction;
+
+namespace GeneralStoreMVC.Models.Customer
+{
+    public class CustomerDetailViewModel
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public List<TransactionListItem> Transactions { get; set; } = new();
+    }
+}
---
+++ Models/Product/ProductIndexVM.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GeneralStoreMVC.Models.Product;
+
+namespace GeneralStoreMVC.Models.Product
+{
+    public class ProductIndexVM
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int QuantityInStock { get; set; }
+    }
+}
